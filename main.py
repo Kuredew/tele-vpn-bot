c34e2aed5d26ba29d17f1207d4d67df20d7fb106
Kureichi <burrohmanhabib35@gmail.com>
2025-08-17 20:23:40 +0700

    refactor: use webhook with flask

--- main.py
+++ main.py
@@ -1,11 +1,24 @@
+import config
 from core.bot import bot
 from handlers.message import message_handler
+from telebot import types
 
-def launch():
-    message_handler.register_handler(bot)
+from flask import Flask, request
 
-    print("MAIN : Bot Started")
-    bot.polling()
+app = Flask(__name__)
 
-if __name__ == "__main__":
-    launch()
+bot.remove_webhook()
+bot.set_webhook(url=config.WEBHOOK_URL)
+
+@app.route("/", methods=['POST'])
+def update():
+
+    json = request.get_data().decode('utf-8')
+    updates = types.Update.de_json(json)
+
+    bot.process_new_updates([updates])
+    return 'ok', 200
+
+
+message_handler.register_handler(bot)
+print("MAIN : Bot Started")
