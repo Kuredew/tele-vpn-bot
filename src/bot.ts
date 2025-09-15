d192005adf0a2589daa526ea448c5ad3af6caeb9
Kuredew <burrohmanhabib35@gmail.com>
2025-09-15 16:48:02 +0700

    fix: Not found certificate and error building to js

--- src/bot.ts
+++ src/bot.ts
@@ -1,11 +1,12 @@
 import { Telegraf } from "telegraf"
-import { BOT_TOKEN, CERTIFICATE_PATH, MONGODB_DATABASE_URL, PRIVATE_KEY_PATH, SERVER_PORT, WEBHOOK_DOMAIN } from "config"
+import { BOT_TOKEN, CA_BUNDLE_PATH, CERTIFICATE_PATH, MONGODB_DATABASE_URL, PRIVATE_KEY_PATH, SERVER_PORT, WEBHOOK_DOMAIN } from "config"
 import { exit } from "process"
 
 import registerHandlers from "registerHandlers"
 import mongoose from "mongoose"
 import express from "express"
 import https from "https"
+import { readFileSync } from "fs"
 
 if (!BOT_TOKEN || !MONGODB_DATABASE_URL || !SERVER_PORT) {
     console.log("ABORTED. Make sure you've set .env correctly")
@@ -20,17 +21,16 @@ registerHandlers(bot)
 
 mongoose.connect(dbURL)
     .then(async () => {
-        if (!WEBHOOK_DOMAIN) {
-            console.log('ABORTED. WEBHOOK_DOMAIN IS NOT VALID.')
+        if (!WEBHOOK_DOMAIN || !PRIVATE_KEY_PATH || !CERTIFICATE_PATH || !CA_BUNDLE_PATH) {
+            console.log('ABORTED. .ENV IS NOT VALID.')
             return
         }
 
-        const webhookPath = await bot.createWebhook( { domain: WEBHOOK_DOMAIN })
-
-        app.use(webhookPath)
+        app.use(await bot.createWebhook( { domain: WEBHOOK_DOMAIN }))
         const server = https.createServer({
-            key: PRIVATE_KEY_PATH,
-            cert: CERTIFICATE_PATH
+            key: readFileSync(PRIVATE_KEY_PATH, 'utf-8'),
+            cert: readFileSync(CERTIFICATE_PATH, 'utf-8'),
+            ca: readFileSync(CA_BUNDLE_PATH, 'utf-8')
         }, app)
 
         server.listen(SERVER_PORT, () => {
--- src/config.ts
+++ src/config.ts
@@ -10,3 +10,4 @@ export const SERVER_PORT = process.env.SERVER_PORT
 
 export const PRIVATE_KEY_PATH = process.env.PRIVATE_KEY_PATH
 export const CERTIFICATE_PATH = process.env.CERTIFICATE_PATH
+export const CA_BUNDLE_PATH = process.env.CA_BUNDLE_PATH
